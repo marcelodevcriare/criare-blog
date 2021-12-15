dfb8cc12420ab7459d2f763a6c2093ae6f2c714a
Wenderson Fazan <wenderson@criarenet.com>
2021-12-15 13:18:22 -0300

    ajuste traduções

--- publishable/lang/pt_br/dimmer.php
+++ publishable/lang/pt_br/dimmer.php
@@ -6,8 +6,8 @@ return [
     'page_text'               => 'Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todas as páginas.',
     'post'                    => 'Publicação|Publicações',
     'post_link_text'          => 'Ver todas as publicações',
-    'post_text'               => 'Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todas as publicações.',
-    'user'                    => 'Utilizador|Utilizadores',
-    'user_link_text'          => 'Ver todos os utilizadores',
-    'user_text'               => 'Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todos os utilizadores.',
+    'post_text'               => 'Tem :count :string no seu blog. Clique no botão abaixo para ver todas as publicações.',
+    'user'                    => 'Usuário|Usuários',
+    'user_link_text'          => 'Ver todos os Usuários',
+    'user_text'               => 'Tem :count :string cadastrados. Clique no botão abaixo para ver todos os usuários.',
 ];
